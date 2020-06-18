6359b7982b323d44ac3e72fd47be89445c15d459
John Larsen <jr.larsen@mac.com>
2020-06-18 11:30:18 +0100

    update bookings page to show week picker

--- src/components/Bookings/weekReducer.js
+++ src/components/Bookings/weekReducer.js
@@ -1,41 +1,16 @@
-import React, {useReducer} from "react";
-import reducer from "./weekReducer";
-import {getWeek} from "../../utils/date-wrangler";
-import {FaChevronLeft, FaCalendarDay, FaChevronRight} from "react-icons/fa";
+import { getWeek } from "../../utils/date-wrangler";
 
-export default function WeekPicker ({date}) {
-  const [week, dispatch] = useReducer(reducer, date, getWeek);
-
-  return (
-    <div>
-      <p className="date-picker">
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "PREV_WEEK"})}
-        >
-          <FaChevronLeft/>
-          <span>Prev</span>
-        </button>
-
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "TODAY"})}
-        >
-          <FaCalendarDay/>
-          <span>Today</span>
-        </button>
-
-        <button
-          className="btn"
-          onClick={() => dispatch({type: "NEXT_WEEK"})}
-        >
-          <span>Next</span>
-          <FaChevronRight/>
-        </button>
-      </p>
-      <p>
-        {week.start.toDateString()} - {week.end.toDateString()}
-      </p>
-    </div>
-  );
+export default function reducer (state, action) {
+  switch (action.type) {
+    case "NEXT_WEEK":
+      return getWeek(state.date, 7);
+    case "PREV_WEEK":
+      return getWeek(state.date, -7);
+    case "TODAY":
+      return getWeek(new Date());
+    case "SET_DATE":
+      return getWeek(new Date(action.payload));
+    default:
+      throw new Error(`Unknown action type: ${action.type}`)
+  }
 }
