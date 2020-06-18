2fbd0b3924827e7301501e1b272bcee92e5485d7
John Larsen <jr.larsen@mac.com>
2020-06-18 16:50:36 +0100

    load data for the user picker

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,7 +1,21 @@
-import React from "react";
-import {users} from "../../static.json";
+import React, {useEffect, useState} from "react";
+import {FaSpinner} from "react-icons/fa";
 
 export default function UserPicker() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+
+    fetch("http://localhost:3001/users")
+      .then(resp => resp.json())
+      .then(data => setUsers(data));
+
+  }, []);
+
+  if (users === null) {
+    return <FaSpinner className="icon-loading"/>
+  }
+
   return (
     <select>
       {users.map(u => (
